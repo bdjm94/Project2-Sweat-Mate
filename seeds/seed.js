926dc92ced00990fb41de3e2c28090f0eddac757
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-10 11:22:10 +1000

    adding more code in seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,12 @@
 const sequelize = require('../config/connection');
-const { User, Blog } = require('../models');
+const { User, Blog, Stats, Comment, Workout, Exercise } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
+const statsData = require('./stats-seeds.json');
+const commentData = require('./commentData.json');
+const workoutData = require('./workout-seeds.json');
+const exerciseData = require('./exercise-seeds.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -18,6 +22,21 @@ const seedDatabase = async () => {
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
+  const stats = await Stats.bulkCreate(statsData, {
+    returning: true,
+  });
+
+  const comments = await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+
+  const workouts = await Workout.bulkCreate(workoutData, {
+    returning: true,
+  });
+
+  const exercise = await Exercise.bulkCreate(exerciseData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
