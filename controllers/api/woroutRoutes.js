88eb461aee4977939d25f5f1585569d7d6091fbd
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-10 11:23:27 +1000

    changing the wourkout routes to match the model

---
+++ controllers/api/woroutRoutes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Workout, Exercise } = require('../../models');
+const auth = require('../../utils/auth');
+
+// This allows you to addd workouts
+router.post('/add', auth, async (req, res) => {
+  try {
+    const newWorkout = await Workout.create({
+      title: req.body.title,
+      description: req.body.description,
+      user_id: req.session.user_id
+    });
+    res.status(200).json(newWorkout);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// This allows you to update workouts
+router.put('/update/:id', auth, async (req, res) => {
+  try {
+    const workoutUpdate = await Workout.update(
+      {
+        title: req.body.title,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id
+        },
+      }
+    );
+
+    res.status(200).json(workoutUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//   This allows you to delete workouts
+router.delete('/delete/:id', auth, async (req, res) => {
+  try {
+
+    const exerciseDelete = await Exercise.destroy({
+      where: {
+        workout_id: req.params.id,
+      }
+    });
+    res.status(200).json(exerciseDelete);
+    const workoutDelete = await Workout.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id
+      }
+    });
+
+    res.status(200).json(workoutDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
