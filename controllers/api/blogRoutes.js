74df5dfa97b3a8e64c3bbea33de5e9ad9135447d
Stephanie Lee <stephaniemjlee@gmail.com>
2021-06-13 01:17:19 +1000

    updated handlebars

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Blog } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/blog', async (req, res) => {
   try {
     const newBlog = await Blog.create({
       name: req.body.name,
