176627df9e47826e33683a11483e5daa174ab43b
Stephanie Lee <stephaniemjlee@gmail.com>
2021-06-14 23:57:12 +1000

    updated files

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
@@ -33,6 +33,49 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get('/:id', async (req, res) => {
+try {
+  const blogData = await Blog.findByPk(req.params.id, {
+    include: [{ model: User }]
+// find one category by its `id` value
+// be sure to include its associated Products
+  });
+
+  if (!blogData) {
+  res.status(404).json({ message: 'No post found with this id!' });
+  return;
+  }
+
+  res.status(200).json(blogData);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+router.get('/:id', (req, res) => {
+  Blog.findOne({
+          where: {
+              id: req.params.id
+          },
+          attributes: ['id',
+              'description',
+              'name',
+              'date_created'
+          ],
+      })
+      .then(dbBlogData => {
+          if (!dbBlogData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+          res.json(dbBlogData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 module.exports = router;
